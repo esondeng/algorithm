f3ca17c0776e52ab4ffc5326d0eca212a2f04107
dengxiaolin <dreamdlx@163.com>
2021-06-04 12:32:32 +0800

    最大堆，最小堆

---
+++ src/main/java/com/test/algorithm/MedianFinder.java
@@ -0,0 +1,46 @@
+package com.test.algorithm;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.stream.Stream;
+
+/**
+ * Find Median from Data Stream
+ * 寻找中位数
+ * 思路，左边最大堆，右边最小堆
+ * 进来的数据，先放最大堆
+ *
+ * @author dengxiaolin
+ * @since 2021/06/04
+ */
+public class MedianFinder {
+
+    /**
+     * 右边
+     */
+    static PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+    /**
+     * 左边
+     */
+    static PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+    public static void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    public static double findMedian() {
+        return maxHeap.size() == minHeap.size() ? (double) (minHeap.peek() + maxHeap.peek()) / 2D : maxHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        Stream.iterate(0, i -> i + 1)
+                .limit(5)
+                .forEach(MedianFinder::addNum);
+        System.out.println(findMedian());
+    }
+}
