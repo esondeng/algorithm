08427a4949bd4595628fcc3945296d3381d692c1
dengxiaolin <dreamdlx@163.com>
2021-06-17 14:09:28 +0800

    中位数

--- src/main/java/com/test/algorithm/MedianFinder.java
+++ src/main/java/com/test/algorithm/MedianFinder.java
@@ -6,7 +6,7 @@ import java.util.PriorityQueue;
 /**
  * Find Median from Data Stream
  * 寻找中位数
- * 思路，左边最大堆，右边最小堆
+ * 思路，左边最大堆，右边最小堆， 利用JAVA的PriorityQueue，用数组来实现的最小堆。
  * 进来的数据，先放最大堆
  *
  * @author dengxiaolin
